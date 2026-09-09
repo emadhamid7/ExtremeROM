9464a7a868b967a85cd0b62b6090d5695295eab3
Emad <emadhamid070@gmail.com>
2026-09-09 22:34:42 +0530

    Refactor build display ID settings in customize.sh
    Removed unnecessary properties from build display ID settings.

--- unica/patches/miscs/customize.sh
+++ unica/patches/miscs/customize.sh
@@ -5,11 +5,7 @@ REGION=$(echo -n "$TARGET_FIRMWARE" | cut -d "/" -f 2)
 ROM_STATUS=""
 $ROM_IS_OFFICIAL || ROM_STATUS=" UNOFFICIAL"
 VALUE="$(GET_PROP "$WORK_DIR/system/system/build.prop" "ro.build.display.id")"
-SET_PROP "system" "ro.build.display.id" "ExtremeROM$ROM_STATUS $ROM_CODENAME $ROM_VERSION - $TARGET_CODENAME ($VALUE)"
-
-SET_PROP "system" "ro.extremerom.official" "$ROM_IS_OFFICIAL"
-SET_PROP "system" "ro.extremerom.version" "$ROM_VERSION"
-SET_PROP "system" "ro.extremerom.codename" "$ROM_CODENAME"
+SET_PROP "system" "ro.build.display.id" "ExtremeROM $ROM_CODENAME $ROM_VERSION - ($VALUE)"
 
 # Disable FRP
 SET_PROP "vendor" "ro.frp.pst" ""
