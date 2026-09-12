56b0dd5e6db3dcb4f0550e98821e10db78f99e8f
Emad <emadhamid070@gmail.com>
2026-09-12 16:03:23 +0530

    tiny fix

--- platform/exynos9820/patches/stock_blobs/customize.sh
+++ platform/exynos9820/patches/stock_blobs/customize.sh
@@ -6,14 +6,15 @@ REGION=$(echo -n "$TARGET_FIRMWARE" | cut -d "/" -f 2)
 # S10 Series -> SoundBooster 1000
 LOG_STEP_IN "- Replacing SoundBooster"
 DELETE_FROM_WORK_DIR "system" "system/lib/lib_SoundBooster_ver1050.so"
-DELETE_FROM_WORK_DIR "system" "system/lib/libsoundboostereq_legacy.so"
+DELETE_FROM_WORK_DIR "system" "system/lib/libsamsungSoundbooster_plus_legacy.so"
 DELETE_FROM_WORK_DIR "system" "system/lib64/lib_SoundBooster_ver1050.so"
-DELETE_FROM_WORK_DIR "system" "system/lib64/libsoundboostereq_legacy.so"
+DELETE_FROM_WORK_DIR "system" "system/lib64/libsamsungSoundbooster_plus_legacy.so"
 ADD_TO_WORK_DIR "$TARGET_FIRMWARE" "system" "system/lib/lib_SoundBooster_ver1000.so" 0 0 644 "u:object_r:system_lib_file:s0"
 ADD_TO_WORK_DIR "$TARGET_FIRMWARE" "system" "system/lib/libsamsungSoundbooster_plus_legacy.so" 0 0 644 "u:object_r:system_lib_file:s0"
 ADD_TO_WORK_DIR "$TARGET_FIRMWARE" "system" "system/lib64/lib_SoundBooster_ver1000.so" 0 0 644 "u:object_r:system_lib_file:s0"
 ADD_TO_WORK_DIR "$TARGET_FIRMWARE" "system" "system/lib64/libsamsungSoundbooster_plus_legacy.so" 0 0 644 "u:object_r:system_lib_file:s0"
 LOG_STEP_OUT
+
 
 LOG_STEP_IN "- Replacing GameDriver"
 ADD_TO_WORK_DIR "$TARGET_FIRMWARE" "system" "system/priv-app/GameDriver-EX9820/GameDriver-EX9820.apk" 0 0 644 "u:object_r:system_file:s0"
