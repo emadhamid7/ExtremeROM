c284d705d030ef5b1d4cb211cd6439e6424162ee
Emad <emadhamid070@gmail.com>
2026-09-16 14:11:54 +0530

    Refactor config application function in customize.sh

--- unica/patches/floating_feature/customize.sh
+++ unica/patches/floating_feature/customize.sh
@@ -1,27 +1,35 @@
-READ_AND_APPLY_CONFIGS()
+APPLY_CUSTOM_FEATURE()
 {
-    local CONFIG_FILE="$SRC_DIR/target/$TARGET_CODENAME/sff.sh"
+    local CONFIG_FILE="$1"
+    local ERR_LABEL="$2"
 
-    if [ -f "$CONFIG_FILE" ]; then
-        while read -r i; do
-            [[ "$i" = "#"* ]] && continue
-            [[ -z "$i" ]] && continue
+    while read -r i; do
+        [[ "$i" = "#"* ]] && continue
+        [[ -z "$i" ]] && continue
 
-            if echo -n "$i" | grep -q "="; then
-                if [[ -z "$(echo -n "$i" | cut -d "=" -f 2)" ]]; then
-                    SET_FLOATING_FEATURE_CONFIG "$(echo -n "$i" | cut -d "=" -f 1)" --delete
-                else
-                    SET_FLOATING_FEATURE_CONFIG "$(echo -n "$i" | cut -d "=" -f 1)" "$(echo -n "$i" | cut -d "=" -f 2-)"
-                fi
+        if echo -n "$i" | grep -q "="; then
+            if [[ -z "$(echo -n "$i" | cut -d "=" -f 2)" ]]; then
+                SET_FLOATING_FEATURE_CONFIG "$(echo -n "$i" | cut -d "=" -f 1)" --delete
             else
-                LOGE "Malformed string in target/\"$TARGET_CODENAME\"/sff.sh: \"$i\""
+                SET_FLOATING_FEATURE_CONFIG "$(echo -n "$i" | cut -d "=" -f 1)" "$(echo -n "$i" | cut -d "=" -f 2-)"
             fi
-        done < "$CONFIG_FILE"
-    fi
+        else
+            LOGE "Malformed string in $ERR_LABEL: \"$i\""
+        fi
+    done < "$CONFIG_FILE"
 }
 
-# Apply target floating feature
-READ_AND_APPLY_CONFIGS
+if [ -f "$SRC_DIR/platform/$TARGET_PLATFORM/sff.sh" ]; then
+    LOG_STEP_IN "- Applying custom platform floating feature config"
+    APPLY_CUSTOM_FEATURE "$SRC_DIR/platform/$TARGET_PLATFORM/sff.sh" "platform/\"$TARGET_PLATFORM\"/sff.sh"
+    LOG_STEP_OUT
+fi
+
+if [ -f "$SRC_DIR/target/$TARGET_CODENAME/sff.sh" ]; then
+    LOG_STEP_IN "- Applying custom device floating feature config"
+    APPLY_CUSTOM_FEATURE "$SRC_DIR/target/$TARGET_CODENAME/sff.sh" "target/\"$TARGET_CODENAME\"/sff.sh"
+    LOG_STEP_OUT
+fi
 
 # Smart Tutor
 SET_FLOATING_FEATURE_CONFIG "SEC_FLOATING_FEATURE_COMMON_CONFIG_SMARTTUTOR_PACKAGES_PATH" --delete
