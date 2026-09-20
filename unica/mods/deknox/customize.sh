c13868d78e8b498a3f072041d86ff25953e0b81b
Emad <emadhamid070@gmail.com>
2026-09-20 23:46:21 +0530

    Update customize.sh

--- unica/mods/deknox/customize.sh
+++ unica/mods/deknox/customize.sh
@@ -1,3 +1,4 @@
+
 # KnoxGuard
 DELETE_FROM_WORK_DIR "system" "system/priv-app/KnoxGuard"
 DELETE_FROM_WORK_DIR "system" "system/etc/permissions/privapp-permissions-com.samsung.android.kgclient.xml"
@@ -6,8 +7,6 @@ DELETE_FROM_WORK_DIR "system" "system/etc/permissions/privapp-permissions-com.sa
 DELETE_FROM_WORK_DIR "system" "system/bin/dualdard"
 DELETE_FROM_WORK_DIR "system" "system/etc/init/dualdard.rc"
 DELETE_FROM_WORK_DIR "system" "system/lib64/libdualdar.so"
-DELETE_FROM_WORK_DIR "system" "system/lib64/aidl_comm_ddar_client.so"
-DELETE_FROM_WORK_DIR "system" "system/lib64/vendor.samsung.hardware.tlc.ddar-V1-ndk.so"
 
 # Blockchain
 DELETE_FROM_WORK_DIR "system" "system/app/BlockchainBasicKit"
@@ -36,19 +35,15 @@ DELETE_FROM_WORK_DIR "system" "system/priv-app/KnoxMposAgent"
 
 # eSE COS
 DELETE_FROM_WORK_DIR "system" "system/bin/sem_daemon"
-DELETE_FROM_WORK_DIR "system" "system/etc/init/sem_early.rc"
 DELETE_FROM_WORK_DIR "system" "system/etc/permissions/privapp-permissions-com.sem.factoryapp.xml"
 DELETE_FROM_WORK_DIR "system" "system/lib64/libsec_sem.so"
-DELETE_FROM_WORK_DIR "system" "system/lib64/libsec_semAidl.so"
 DELETE_FROM_WORK_DIR "system" "system/lib64/libsec_semRil.so"
 DELETE_FROM_WORK_DIR "system" "system/lib64/libsec_semTlc.so"
 DELETE_FROM_WORK_DIR "system" "system/lib64/libspictrl.so"
-DELETE_FROM_WORK_DIR "system" "system/lib64/vendor.samsung.hardware.security.sem-V1-ndk.so"
 DELETE_FROM_WORK_DIR "system" "system/priv-app/SEMFactoryApp"
 
 # Weaver
 DELETE_FROM_WORK_DIR "system" "system/lib64/libhermes_cred.so"
-DELETE_FROM_WORK_DIR "system" "system/lib64/android.hardware.weaver-V2-ndk.so"
 
 # HDM
 DELETE_FROM_WORK_DIR "system" "system/priv-app/HdmApk"
@@ -64,16 +59,9 @@ DELETE_FROM_WORK_DIR "system" "system/etc/permissions/privapp-permissions-com.sa
 DELETE_FROM_WORK_DIR "system" "system/priv-app/KnoxZtFramework"
 
 # Knox Matrix
-DELETE_FROM_WORK_DIR "system" "system/bin/fabric_crypto"
-DELETE_FROM_WORK_DIR "system" "system/etc/init/fabric_crypto.rc"
-DELETE_FROM_WORK_DIR "system" "system/etc/permissions/FabricCryptoLib.xml"
 DELETE_FROM_WORK_DIR "system" "system/etc/permissions/privapp-permissions-com.samsung.android.kmxservice.xml"
-DELETE_FROM_WORK_DIR "system" "system/etc/vintf/manifest/fabric_crypto_manifest.xml"
-DELETE_FROM_WORK_DIR "system" "system/framework/FabricCryptoLib.jar"
-DELETE_FROM_WORK_DIR "system" "system/lib64/com.samsung.security.fabric.cryptod-V1-cpp.so"
 DELETE_FROM_WORK_DIR "system" "system/lib64/vendor.samsung.hardware.security.fkeymaster-V1-cpp.so"
 DELETE_FROM_WORK_DIR "system" "system/lib64/vendor.samsung.hardware.security.fkeymaster-V1-ndk.so"
-DELETE_FROM_WORK_DIR "system" "system/priv-app/KmxService"
 
 # Other Knox APKs
 DELETE_FROM_WORK_DIR "system" "system/priv-app/KPECore"
