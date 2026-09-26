f7f13ab3551360648dbee432376850b27b958aaa
Emad <emadhamid070@gmail.com>
2026-09-26 10:19:51 +0530

    Update SoundBooster version in customize.sh

--- platform/exynos9820/patches/stock_blobs/customize.sh
+++ platform/exynos9820/patches/stock_blobs/customize.sh
@@ -5,9 +5,9 @@ REGION=$(echo -n "$TARGET_FIRMWARE" | cut -d "/" -f 2)
 # S21 OneUI 7 -> SoundBooster 1050
 # S10 Series -> SoundBooster 1000
 LOG_STEP_IN "- Replacing SoundBooster"
-DELETE_FROM_WORK_DIR "system" "system/lib/lib_SoundBooster_ver1050.so"
+DELETE_FROM_WORK_DIR "system" "system/lib/lib_SoundBooster_ver1100.so"
 DELETE_FROM_WORK_DIR "system" "system/lib/libsamsungSoundbooster_plus_legacy.so"
-DELETE_FROM_WORK_DIR "system" "system/lib64/lib_SoundBooster_ver1050.so"
+DELETE_FROM_WORK_DIR "system" "system/lib64/lib_SoundBooster_ver1100.so"
 DELETE_FROM_WORK_DIR "system" "system/lib64/libsamsungSoundbooster_plus_legacy.so"
 ADD_TO_WORK_DIR "$TARGET_FIRMWARE" "system" "system/lib/lib_SoundBooster_ver1000.so" 0 0 644 "u:object_r:system_lib_file:s0"
 ADD_TO_WORK_DIR "$TARGET_FIRMWARE" "system" "system/lib/libsamsungSoundbooster_plus_legacy.so" 0 0 644 "u:object_r:system_lib_file:s0"
